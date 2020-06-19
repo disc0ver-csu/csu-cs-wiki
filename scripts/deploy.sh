ceddbe35236ed305ccbdd20311e3c57b65e7bb86
leo6033 <long452a@163.com>
2020-06-19 13:13:25 +0800

    travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
  # @Description: 
  # @Author: 妄想
  # @Date: 2020-06-19 12:37:24
- # @LastEditTime: 2020-06-19 12:39:30
+ # @LastEditTime: 2020-06-19 13:13:04
  # @LastEditors: 妄想
 ### 
 #!/usr/bin/env bash
@@ -12,18 +12,14 @@
 ORG=leo6033
 REPO=CSU_CS_Experiment
 # This probably should match an email for one of your users.
-EMAIL=csulmw@163.com
+EMAIL=long452a@163.com
 
 set -e
 
 git remote add gh-token "https://${GH_TOKEN}@github.com/$ORG/$REPO.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 
-# Update git configuration so I can push.
-if [ "$1" != "dry" ]; then
-    # Update git config.
-    git config user.name "csu-cs-wiki"
-    git config user.email "$EMAIL"
-fi
+git config user.name "leo6033"
+git config user.email "$EMAIL"
 
 mkdocs gh-deploy -v --clean --remote-name gh-token;
