1625cad79d3da44a5c39d044e0d2e76f724b69d3
WeipengZhang <wp.zhang@hotmail.com>
2020-07-04 11:37:50 +0800

    add Java project - Chat room

---
+++ 实验课设代码/Java/课程设计-聊天室/code/PP/src/Server/ServerThread.java
@@ -0,0 +1,30 @@
+package Server;
+
+import utilities.Operations;
+import utilities.ServerOperations;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerThread extends Thread{
+    private Socket s;
+    private ServerSocket ss;
+    ServerOperations operator;
+
+    public ServerThread(Socket s, ServerSocket ss, ServerOperations operator){
+        this.s = s;
+        this.ss = ss;
+        this.operator = operator;
+    }
+
+    @Override
+    public void run(){
+        while (true) {
+            try {
+                //接收来自客户端的消息
+                operator.ServerReceiveMsg(ss, s);
+            } catch (Exception e) {}
+        }
+    }
+}
